a31ea2b4e2639226bb41256a3260e6ffc989ace8
Yasin Walum <63132133+wyasyn@users.noreply.github.com>
2023-08-20 02:36:04 +0300

    navbar disappear

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react';
 import './Navbar.scss'
 import { NavDesk, NavMobile } from '../../components'
 
 function Navbar() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [prevScrollY, setPrevScrollY] = useState(window.scrollY);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setIsVisible(prevScrollY > currentScrollY);
+    setPrevScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollY]);
   return (
-    <header className="navbar">
+    <header className={isVisible ? 'navbar' : 'navbar hidden'}>
       <NavMobile />
       <NavDesk />
     </header>
